441a18b28087ba9326305618a8329cd572dd7c64
Adam Carnaffan <acarnaffan@hotmail.com>
2019-10-16 11:06:30 -0400

    Finished part2

--- Lab_5_prelab.v
+++ Lab_5_prelab.v
@@ -54,17 +54,74 @@ module adder(A, B, Cin, S, Cout); // Copied from lab4 part3
     assign S = (~Cin & ((~A & B) | (A & ~B))) | (Cin & ((~A & ~B) | (A & B)));
 endmodule
 
-module disp_8bit_hex(value, Disp1, Disp2); // Disp1 is the right-most display
+module disp_4bit_hex(C, Disp);
     // Get inputs
-    
+    input [7:0] C;
+    output [6:0] Disp;
+    // Make assignments
+    assign Disp[0] = (C[2] & ~C[1] & ~C[0]) | (C[0] & ((C[3] & (C[2] ^ C[1])) | (~C[1] & ~C[2] & ~C[3])));
+    assign Disp[1] = (C[3] & ((C[1] & C[0]) | (C[2] & ~C[0]))) | (~C[3] & C[2] & (C[1] ^ C[0]));
+    assign Disp[2] = (C[3] & C[2] & (C[1] | (~C[1] & ~C[0]))) | (~C[3] & ~C[2] & C[1] & ~C[0]);
+    assign Disp[3] = (C[1] & ((C[2] & C[0]) | (C[3] & ~C[2] & ~C[0]))) | (~C[3] & ~C[1] & ((C[2] & ~C[0]) | (~C[2] & C[0])));
+    assign Disp[4] = (C[0] & ((~C[3]) | (C[3] & ~C[1]))) | (~C[3] & C[2] & ~C[1] & ~C[0]);
+    assign Disp[5] = (C[3] & C[2] & ~C[1]) | (~C[3] & ~C[2] & (C[1] | C[0]) | (~C[3] & C[2] & C[1] & C[0]));
+    assign Disp[6] = ~C[3] & ((~C[2] & ~C[1]) | (C[2] & C[1] & C[0]));
 endmodule
 
-module 
+module part2(SW, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
+    // Get inputs
+    input [8:0] SW;
+    output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+    output LEDR;
+    reg [7:0] A, B;
+    wire [7:0] S, C;
+    // Save A
+    always @(posedge SW[8])
+    begin
+        A <= SW[7:0];
+    end
+    // Save B
+    always @(negedge SW[8])
+    begin
+        B <= SW[7:0];
+    end
+    // Calculate
+    adder A0 (A[0], B[0], 1'b0, S[0], C[0]);
+    adder A1 (A[1], B[1], C[0], S[1], C[1]);
+    adder A2 (A[2], B[2], C[1], S[2], C[2]);
+    adder A3 (A[3], B[3], C[2], S[3], C[3]);
+    adder A4 (A[4], B[4], C[3], S[4], C[4]);
+    adder A5 (A[5], B[5], C[4], S[5], C[5]);
+    adder A6 (A[6], B[6], C[5], S[6], C[6]);
+    adder A7 (A[7], B[7], C[6], S[7], C[7]);
+    // Display
+    disp_4bit_hex D0 (A[3:0], HEX0);
+    disp_4bit_hex D1 (A[7:4], HEX1);
+    disp_4bit_hex D2 (B[3:0], HEX2);
+    disp_4bit_hex D3 (B[7:4], HEX3);
+    disp_4bit_hex D4 (S[3:0], HEX4);
+    disp_4bit_hex D5 (S[7:4], HEX5);
+endmodule
 
 //////////////
 /// Part 3 ///
 //////////////
 
+module disp_4bit_hex(C, Disp);
+    // Get inputs
+    input [7:0] C;
+    output [6:0] Disp;
+    // Make assignments
+    assign Disp[0] = (C[2] & ~C[1] & ~C[0]) | (C[0] & ((C[3] & (C[2] ^ C[1])) | (~C[1] & ~C[2] & ~C[3])));
+    assign Disp[1] = (C[3] & ((C[1] & C[0]) | (C[2] & ~C[0]))) | (~C[3] & C[2] & (C[1] ^ C[0]));
+    assign Disp[2] = (C[3] & C[2] & (C[1] | (~C[1] & ~C[0]))) | (~C[3] & ~C[2] & C[1] & ~C[0]);
+    assign Disp[3] = (C[1] & ((C[2] & C[0]) | (C[3] & ~C[2] & ~C[0]))) | (~C[3] & ~C[1] & ((C[2] & ~C[0]) | (~C[2] & C[0])));
+    assign Disp[4] = (C[0] & ((~C[3]) | (C[3] & ~C[1]))) | (~C[3] & C[2] & ~C[1] & ~C[0]);
+    assign Disp[5] = (C[3] & C[2] & ~C[1]) | (~C[3] & ~C[2] & (C[1] | C[0]) | (~C[3] & C[2] & C[1] & C[0]));
+    assign Disp[6] = ~C[3] & ((~C[2] & ~C[1]) | (C[2] & C[1] & C[0]));
+endmodule
+
+module part3(SW, KEY, HEX0, HEX1, HEX2, HEX3)
 
 //////////////
 /// Part 4 ///
