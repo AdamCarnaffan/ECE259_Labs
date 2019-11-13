3da247b287e93347070e6a422a2de2b3493dcd34
Adam Carnaffan <acarnaffan@hotmail.com>
2019-11-13 00:39:54 -0500

    Clobbering error exists

--- Lab_8_prelab.s
+++ Lab_8_prelab.s
@@ -85,22 +85,33 @@ CHECK_WORD:
     CMP R0, #-1 ; End on list delimiter
     BEQ END
 
-    MOV R1, #0 ; Set the counter for the number of 1s to zero
+    MOV R1, #0 ; Set the counter for the number of ones
+    MOV R2, #0x80000000 ; Default to the first bit and then shift
+    MOV R3, #0 ; Set iterator to 0 to verify completion
+    PUSH {R6-R10}
     BL COUNT_ONES ; Move to the word initiator
+    POP {R6-R10}
     CMP R8, R1 ; Compare longest current word to longest longest
     MOVLE R8, R1 ; If R1 is big boi, make the swap
 
     LDR R0, [R6, R7] ; Reload state
-    MOV R3, #0 ; Set iterator to 0 to verify a complete set of zeros
     MOV R1, #0 ; Set the counter for the number of leading to zero
+    MOV R2, #0x80000000 ; Default to the first bit and then shift
+    MOV R3, #0 ; Set iterator to 0 to verify a complete set of zeros
+    PUSH {R6-R10}
     BL COUNT_LEADING ; Move to the leading counter
+    POP {R6-R10}
     CMP R9, R1 ; Compare longest current word to longest longest
     MOVLE R9, R1 ; If R1 is big boi, make the swap
     
     ; Dont reload state because we move from the end
+    LDR R0, [R6, R7] ; Reload state
+    MOV R1, #0 ; Set the counter for the number of leading to zero
+    MOV R2, #0x00000001 ; Default to the first bit and then shift
     MOV R3, #0 ; Set iterator to 0 to verify a complete set of zeros
-    MOV R1, #0 ; Set the counter for the number of  trailing to zero
+    PUSH {R6-R10}
     BL COUNT_TRAILING ; Move to the trailing counter
+    POP {R6-R10}
     CMP R10, R1 ; Compare longest current word to longest longest
     MOVLE R10, R1 ; If R1 is big boi, make the swap
 
@@ -108,34 +119,39 @@ CHECK_WORD:
     B CHECK_WORD ; Continue looping word checks
 
 COUNT_LEADING:
-    ADD R3, #1 ; Increment number of bits counted
-    CMP R0, #1 ; loop until the data contains no more 1’s
-    MOVEQ PC, LR ; Go back to the iterator evaluation   
-    LSR R2, R0, #1 ; perform SHIFT, followed by AND
-    AND R0, R0, R2
-    ADD R1, #1 ; count the string lengths so far
-    CMP R3, #32 ; Check if value is greater than number of bits per word
-    MOVGE PC, LR ; Go back to the iterator evaluation
-    B COUNT_LEADING ; go back to the top of the word
+    MOV R5, R0 ; Load first position again
+    LSL R5, R3 ; Shift by the iterator's count
+    AND R5, R5, R2 ; And the MSB with the shifted value
+    CMP R5, #0
+    MOVNE PC, LR ; Move back if the value is not zero
+    CMP R3, #32
+    MOVEQ PC, LR ; Move back if full string is 0s
+    ADD R3, #1 ; Iterate iterator
+    ADD R1, #1 ; Iterate the counter
+    B COUNT_LEADING
 
 COUNT_TRAILING:
-    ADD R3, #1 ; Increment number of bits counted
-    CMP R0, #1 ; loop until the data contains no more 1’s
-    MOVEQ PC, LR ; Go back to the iterator evaluation   
-    LSL R2, R0, #1 ; perform SHIFT, followed by AND
-    AND R0, R0, R2
-    ADD R1, #1 ; count the string lengths so far
-    CMP R3, #32 ; Check if value is greater than number of bits per word
-    MOVGE PC, LR ; Go back to the iterator evaluation
-    B COUNT_TRAILING ; go back to the top of the word
+    MOV R5, R0 ; Load first position again
+    LSR R5, R3 ; Shift by the iterator's count
+    AND R5, R5, R2 ; And the MSB with the shifted value
+    CMP R5, #0
+    MOVNE PC, LR ; Move back if the value is not zero
+    CMP R3, #32
+    MOVEQ PC, LR ; Move back if full string is 0s
+    ADD R3, #1 ; Iterate iterator
+    ADD R1, #1 ; Iterate the counter
+    B COUNT_TRAILING
 
 COUNT_ONES:
-    CMP R0, #0 ; loop until the data contains no more 1’s
-    MOVEQ PC, LR ; Go back to the iterator evaluation
-    LSR R2, R0, #1 ; perform SHIFT, followed by AND
-    AND R0, R0, R2
-    ADD R1, #1 ; count the string lengths so far
-    B COUNT_ONES ; go back to the top of the word
+    MOV R5, R0 ; Load first position again
+    LSL R5, R3 ; Shift by the iterator's count
+    AND R5, R5, R2 ; And the MSB with the shifted value
+    CMP R5, #0
+    ADDNE R1, #1 ; Add to counter if 1
+    CMP R3, #32
+    MOVEQ PC, LR ; Move back if full string is 0s
+    ADD R3, #1 ; Iterate iterator
+    B COUNT_ONES ; Loop
 
 END: B END ; End loop
 
